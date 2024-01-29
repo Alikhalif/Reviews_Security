78670cebdc6c3e64da7493a47499a84ac178b8fb
Essoulihamza <ehamza.web@gmail.com>
2024-01-29 09:56:05 +0100

    setup

---
+++ src/main/java/ma/youcode/youreview/YoureviewApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.youreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoureviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoureviewApplication.class, args);
+	}
+
+}
